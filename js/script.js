0d4e5b15cbb36413635d71b1faf77804bb3d8d8b
Wojciech Glowa <wojciechrajdom@gmail.com>
2021-06-24 21:06:52 +0200

    Blog change

--- js/script.js
+++ js/script.js
@@ -4,17 +4,23 @@
 });*/
 const titleClickHandler = function(){
   console.log('Link was clicked!');
+ 
   
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
+    
   }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts a.active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
@@ -24,8 +30,8 @@ const titleClickHandler = function(){
 }
 
 const links = document.querySelectorAll('.titles a');
-
+console.log(links);
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
-  
+    
 }
