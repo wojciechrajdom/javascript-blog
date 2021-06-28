23cc417d734187dcd605fe6bbdbed05cd6725c40
Wojciech Glowa <wojciechrajdom@gmail.com>
2021-06-28 16:16:42 +0200

    Change in blog

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
-/*document.getElementById('test-button').addEventListener('click', function(){
-  const links = document.querySelectorAll('.titles a');
-  console.log('links:', links);
-});*/
+
 const titleClickHandler = function(){
   console.log('Link was clicked!');
  
   
-  /* remove class 'active' from all article links  */
+  /*  remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-    
+    activeLink.classList.remove('active');  
+  }
+  /* add class 'active' to the clicked link */
+  const clickedlink = function(){
+    clickedlink.classList.add('active');
   }
 
-  /* add class 'active' to the clicked link */
-
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts a.active');
+  const activeArticles = document.querySelectorAll('article');
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
+  const href = function(){
+    console.log('href');
+    clickedlink.classList.remove('href');
+  }
 
   /* find the correct article using the selector (value of 'href' attribute) */
+
 
   /* add class 'active' to the correct article */
 }
