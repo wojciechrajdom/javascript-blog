7802823d2e7c129de7178e2399f12c3f790ae316
Wojciech Glowa <wojciechrajdom@gmail.com>
2021-07-02 18:50:49 +0200

    generateTags

--- js/script.js
+++ js/script.js
@@ -33,8 +33,9 @@ const titleClickHandler = function(){
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
-  
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
+     
 function generateTitleLinks(){
   
   /* remove contents of titleList */
@@ -70,6 +71,37 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  /* START LOOP: for every article: */
+  for(let article of articles){
+    /* find tags wrapper */
+    const tagsWrapper = document.querySelectorAll(optArticleTagsSelector);
+
+    /* make html variable with empty string */
+    
+
+    /* get tags from data-tags attribute */
+    
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+    /* generate HTML of the link */
+
+    /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+    /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
      
 
  
